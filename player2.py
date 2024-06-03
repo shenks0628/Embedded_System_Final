3f00f46bff58c92f624b4524c9acc2f3f50dd80b
shenks0628 <cooljoseph100@gmail.com>
2024-06-03 13:57:48 +0800

    add: player2.py and fix: game logic flow

--- player2.py
+++ player2.py
@@ -8,8 +8,8 @@ xtools.connect_wifi_led()
 ADAFRUIT_IO_USERNAME = "shen115"
 ADAFRUIT_IO_KEY      = "aio_djoS46LGpo3MXlmcpWEvA2T7zfMK"
 STATUS = "status"
-YOUR_NUM = "player1-num"
-OPPONENT_NUM = "player2-num"
+YOUR_NUM = "player2-num"
+OPPONENT_NUM = "player1-num"
 PLAYER1 = "PLAYER1"
 PLAYER2 = "PLAYER2"
 WAITING = 0
@@ -87,11 +87,10 @@ def gameEnd():
 
 def roundEnd():
     global mode, yourHP, opponentHP, opponent_ready
-    # client.publish(status_topic, b"ROUND END")
     print("ROUND END")
     print("YOUR HP: ", yourHP)
     print("OPPONENT HP: ", opponentHP)
-    uart.write(f"GAME:{yourHP}{opponentHP}\r\n")
+    uart.write(f"ROUND:{yourHP}{opponentHP}\r\n")
     uart.sleep(1)
     utime.sleep(5)
     opponent_ready = False
@@ -100,7 +99,7 @@ def roundEnd():
 def playerTurn():
     global mode
     mode = YOUR_TURN
-    print("Player1 Turn")
+    print("Player2 Turn")
     uart.write("TURN\r\n")
     uart.sleep(1)
 
@@ -114,8 +113,8 @@ def check_guess(cnt, num):
             checker += 1
     return checker
 
-def player1WINDBUPD():
-    mes = DATABASE_URL + "/player1.json"
+def player2WINDBUPD():
+    mes = DATABASE_URL + "/player2.json"
     res = urequests.get(mes)
     r = res.text
     res.close()
@@ -126,12 +125,12 @@ def player1WINDBUPD():
         win += data["win"]
         lose += data["lose"]
     data = {"win": win, "lose": lose}
-    mes = DATABASE_URL + "/player1.json"
+    mes = DATABASE_URL + "/player2.json"
     res = urequests.put(mes, json=data)
     res.close()
 
-def player1LoseDBUPD():
-    mes = DATABASE_URL + "/player1.json"
+def player2LOSEDBUPD():
+    mes = DATABASE_URL + "/player2.json"
     res = urequests.get(mes)
     r = res.text
     res.close()
@@ -142,33 +141,33 @@ def player1LoseDBUPD():
         win += data["win"]
         lose += data["lose"]
     data = {"win": win, "lose": lose}
-    mes = DATABASE_URL + "/player1.json"
+    mes = DATABASE_URL + "/player2.json"
     res = urequests.put(mes, json=data)
     res.close()
 
 def win(checker, num):
     global mode, yourHP, opponentHP
     # client.publish(status_topic, b"PLAYER1 WINS")
-    print("PLAYER1 WINS")
+    print("PLAYER2 WINS")
     uart.write(f"WIN:{checker}{num}\r\n")
     uart.sleep(1)
     opponentHP -= 1
     if opponentHP == 0:
         gameEnd()
-        player1WINDBUPD()
+        player2WINDBUPD()
     else:
         roundEnd()
 
 def lose(checker, num):
     global mode, yourHP, opponentHP
     # client.publish(status_topic, b"PLAYER2 WINS")
-    print("PLAYER2 WINS")
+    print("PLAYER1 WINS")
     uart.write(f"LOSE:{checker}{num}\r\n")
     uart.sleep(1)
     yourHP -= 1
     if yourHP == 0:
         gameEnd()
-        player1LoseDBUPD()
+        player2LOSEDBUPD()
     else:
         roundEnd()
 
@@ -178,35 +177,33 @@ def sub_cb(topic, msg):
     print(msg)
     msg = str(msg)
     if topic == status_topic:
-        if msg == "PLAYER2 READY":
+        if msg == "PLAYER1 READY":
             opponents.clear()
             opponent_ready = True
             if opponent_ready and mode == READY:
                 print("GAME START")
                 gameStart()
-                print("PLAYER1 NUMBERS SENT")
+                print("PLAYER2 NUMBERS SENT")
                 print("PLAYER1 TURN")
-                playerTurn()
-        elif msg == "PLAYER2 READY FOR NEXT":
+        elif msg == "PLAYER1 READY FOR NEXT":
             opponents.clear()
             opponent_ready = True
             if opponent_ready and mode == READY:
                 print("ROUND START")
                 roundStart()
-                print("PLAYER1 NUMBERS SENT")
+                print("PLAYER2 NUMBERS SENT")
                 print("PLAYER1 TURN")
-                playerTurn()
-        elif msg[:13] == "PLAYER2 GUESS":
+        elif msg[:13] == "PLAYER1 GUESS":
             current_guess.clear()
             current_guess.insert(msg[15])
             current_guess.insert(msg[16])
-            print("PLAYER2 GUESS:", msg[15:])
+            print("PLAYER1 GUESS:", msg[15:])
             uart.write(f"OPPO:{msg[15:]}\r\n")
             uart.sleep(1)
             utime.sleep(5)
             playerTurn()
-        elif msg == "PLAYER2 CALLS STOP":
-            print("PLAYER2 CALLS STOP")
+        elif msg == "PLAYER1 CALLS STOP":
+            print("PLAYER1 CALLS STOP")
             mode = STOP
             cnt = (current_guess[0] - '0')
             if current_guess[0] == 'A':
@@ -220,7 +217,7 @@ def sub_cb(topic, msg):
     elif topic == opponent_num_topic:
         opponents.clear()
         for i in msg:
-            opponents.insert(i)
+            opponents.insert(i - '0')
         print("Opponent's Numbers:", msg)
 
 client.set_callback(sub_cb)
@@ -240,30 +237,28 @@ while True:
         msg = uart.readline()
         if mode == WAITING and msg == b"READY":
             yours.clear()
-            client.publish(status_topic, b"PLAYER1 READY")
-            print("PLAYER1 READY")
+            client.publish(status_topic, b"PLAYER2 READY")
+            print("PLAYER2 READY")
             mode = READY
             if opponent_ready and mode == READY:
                 print("GAME START")
                 gameStart()
-                print("PLAYER1 NUMBERS SENT")
+                print("PLAYER2 NUMBERS SENT")
                 print("PLAYER1 TURN")
-                playerTurn()
         elif mode == WAIT_NEXT and msg == b"READY":
             yours.clear()
-            client.publish(status_topic, b"PLAYER1 READY FOR NEXT")
-            print("PLAYER1 READY FOR NEXT")
+            client.publish(status_topic, b"PLAYER2 READY FOR NEXT")
+            print("PLAYER2 READY FOR NEXT")
             mode = READY
             if opponent_ready and mode == READY:
                 print("ROUND START")
                 roundStart()
-                print("PLAYER1 NUMBERS SENT")
+                print("PLAYER2 NUMBERS SENT")
                 print("PLAYER1 TURN")
-                playerTurn()
         elif mode == YOUR_TURN:
             if msg == b"STOP":
-                client.publish(status_topic, b"PLAYER1 CALLS STOP")
-                print("PLAYER1 CALLS STOP")
+                client.publish(status_topic, b"PLAYER2 CALLS STOP")
+                print("PLAYER2 CALLS STOP")
                 mode = STOP
                 cnt = (current_guess[0] - '0')
                 if current_guess[0] == 'A':
@@ -281,7 +276,7 @@ while True:
                 current_guess.clear()
                 current_guess.insert(msg[0])
                 current_guess.insert(msg[1])
-                client.publish(status_topic, b"PLAYER1 GUESS: " + msg)
-                print("PLAYER1 GUESS:", msg)
+                client.publish(status_topic, b"PLAYER2 GUESS: " + msg)
+                print("PLAYER2 GUESS:", msg)
                 mode = OPPONENT_TURN
 
